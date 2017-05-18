bdccb7714a1df3a74ffc8d917ea73ad1083f94f9
Sean <seon2you@hotmail.com>
2017-05-18 00:57:38 -0700

    added model and store component

---
+++ src/Ng2SportStoreProject/wwwroot/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: "app",
+    template: `
+        <div class="bg-success p-a-1 text-xs-center">
+            This is Sports Store
+        </div>
+    `
+})
+
+export class AppComponent { }
---
+++ src/Ng2SportStoreProject/wwwroot/app/app.module.ts
@@ -0,0 +1,12 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
